f0e1aaa1e46db95b36622f4e76791f1bf30f1b53
saulhervas <saulhervasgallego@gmail.com>
2024-06-26 23:41:57 +0200

    user register and recover pass

---
+++ app/src/main/java/com/saulhervas/easychat/ui/navigation/UserRecoverPass.kt
@@ -0,0 +1,32 @@
+package com.saulhervas.easychat.ui.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.saulhervas.easychat.R
+import com.saulhervas.easychat.databinding.FragmentRecoverPassBinding
+
+
+class UserRecoverPass : Fragment() {
+
+    private lateinit var binding: FragmentRecoverPassBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRecoverPassBinding.inflate(inflater, container, false)
+        onClickRegister(binding.root)
+        return binding.root
+    }
+
+    fun onClickRegister(view: View) {
+        binding.tvRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_userRecoverPass_to_userRegister)
+        }
+    }
+
+
+}
