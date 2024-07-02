8f1aff9ac49403c17ccff720ba792dfc80984c80
saulhervas <saulhervasgallego@gmail.com>
2024-07-02 17:05:58 +0200

    new fragment and items recicler

--- app/src/main/java/com/saulhervas/easychat/ui/navigation/UserConfig.kt
+++ app/src/main/java/com/saulhervas/easychat/ui/navigation/UserConfig.kt
@@ -6,8 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.saulhervas.easychat.R
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class UserConfig : Fragment() {
 
     override fun onCreateView(
