b3f43266e98271e9ed94d026d28bd3e421e7b28c
saulhervas <saulhervasgallego@gmail.com>
2024-07-04 01:48:53 +0200

    change request

--- app/src/main/java/com/saulhervas/easychat/data/model/modelslogin/LoginRequest.kt
+++ app/src/main/java/com/saulhervas/easychat/data/model/modelslogin/LoginRequest.kt
@@ -3,8 +3,8 @@ package com.saulhervas.easychat.data.model.modelslogin
 import com.google.gson.annotations.SerializedName
 
 data class LoginRequest(
-    @SerializedName("password")
-    val password: String,
     @SerializedName("login")
-    val login: String
+    val login: String,
+    @SerializedName("password")
+    val password: String
 )
--- app/src/main/java/com/saulhervas/easychat/data/model/modelsregister/RegisterRequest.kt
+++ app/src/main/java/com/saulhervas/easychat/data/model/modelsregister/RegisterRequest.kt
@@ -3,8 +3,8 @@ package com.saulhervas.easychat.data.model.modelsregister
 import com.google.gson.annotations.SerializedName
 
 data class RegisterRequest(
-    @SerializedName("password")
-    val password: String,
     @SerializedName("login")
     val login: String,
+    @SerializedName("password")
+    val password: String,
 )
