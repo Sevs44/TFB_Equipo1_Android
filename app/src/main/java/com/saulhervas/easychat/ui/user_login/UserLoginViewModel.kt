1acf0cb6d2b7dd0600713063a075f00279044726
OMRM <oscarmrm14@gmail.com>
2024-07-04 19:52:23 +0200

    Add register user viewModel

---
+++ app/src/main/java/com/saulhervas/easychat/ui/user_login/UserLoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.saulhervas.easychat.ui.user_login
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UserLoginViewModel @Inject constructor(): ViewModel() {
+}
---
+++ app/src/main/java/com/saulhervas/easychat/ui/user_register/UserRegisterViewModel.kt
@@ -0,0 +1,55 @@
+package com.saulhervas.easychat.ui.user_register
+
+import androidx.lifecycle.ViewModel
+import com.saulhervas.easychat.data.model.modelsregister.RegisterRequest
+import com.saulhervas.easychat.data.model.modelsregister.RegisterResponse
+import com.saulhervas.easychat.data.repository.backend.retrofit.ApiClient
+import com.saulhervas.easychat.data.repository.backend.retrofit.ApiService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+
+@HiltViewModel
+class UserRegisterViewModel @Inject constructor(
+
+): ViewModel() {
+    private val loadingMutableState = MutableStateFlow(true)
+    val loadingState: StateFlow<Boolean> = loadingMutableState
+
+    private val errorMutableState = MutableStateFlow("")
+    val errorState: StateFlow<String> = errorMutableState
+
+    private val registerUserMutableState = MutableStateFlow(false)
+    val registerUserState: StateFlow<Boolean> = registerUserMutableState
+
+    private lateinit var apiService: ApiService
+
+    init {
+        apiService = ApiClient.create(ApiService::class.java)
+    }
+    fun registerUser(username: String, password: String) {
+        val registerRequest = RegisterRequest(username, password)
+        val call = apiService.registerUser(registerRequest)
+        call.enqueue(object : Callback<RegisterResponse> {
+            override fun onResponse(
+                call: Call<RegisterResponse>,
+                response: Response<RegisterResponse>
+            ) {
+                if (response.isSuccessful) {
+                    registerUserMutableState.value = true
+                } else {
+                    errorMutableState.value = "Error al crear el usuario"
+                }
+            }
+
+            override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+                errorMutableState.value = "Error al crear el usuario"
+            }
+        })
+    }
+
+}
