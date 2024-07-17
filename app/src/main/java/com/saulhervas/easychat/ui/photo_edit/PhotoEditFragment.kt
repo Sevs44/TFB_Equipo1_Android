76c638b812263404a26d3ea0feb4ed9810e31c5c
Enriquermg21 <enriquermg21@gmail.com>
2024-07-17 17:25:07 +0200

    edit photo,using galery or doing photo and delete photo

--- app/src/main/java/com/saulhervas/easychat/ui/photo_edit/PhotoEditFragment.kt
+++ app/src/main/java/com/saulhervas/easychat/ui/photo_edit/PhotoEditFragment.kt
@@ -1,22 +1,201 @@
 package com.saulhervas.easychat.ui.photo_edit
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.saulhervas.easychat.R
+import com.saulhervas.easychat.databinding.FragmentPhotoEditBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PhotoEditFragment : Fragment() {
 
+    private lateinit var binding: FragmentPhotoEditBinding
+    private lateinit var takePictureLauncher: ActivityResultLauncher<Uri>
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private lateinit var imageUri: Uri
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        observeViewModel()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photo_edit, container, false)
+    ): View {
+        binding = FragmentPhotoEditBinding.inflate(inflater, container, false)
+        setupUI(binding.root)
+        setupCamera()
+        setOnClickListener()
+        return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupUI(view: View) {
+        if (view !is EditText) {
+            view.setOnTouchListener { _, _ ->
+                hideKeyboard()
+                false
+            }
+        }
+
+        if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                val innerView = view.getChildAt(i)
+                setupUI(innerView)
+            }
+        }
+    }
+
+
+    private fun setupCamera() {
+        takePictureLauncher =
+            registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+                if (success) {
+                    Log.d("PhotoEditFragment", "Picture taken successfully")
+                    binding.ivProfile.setImageURI(imageUri)
+                } else {
+                    Log.d("PhotoEditFragment", "Failed to take picture")
+                    Toast.makeText(requireContext(), "Failed to take picture", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    Log.d("PhotoEditFragment", "Camera permission granted")
+                    openCamera()
+                } else {
+                    Log.d("PhotoEditFragment", "Camera permission denied")
+                    Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun setOnClickListener() {
+        binding.btnTakePhoto.setOnClickListener {
+            checkPermissionsAndOpenCamera()
+        }
+        binding.btnDeletePhoto.setOnClickListener {
+            binding.ivProfile.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_launcher_playstore
+                )
+            )
+        }
+        binding.imBtnBack.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+        binding.btnSelectPhoto.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    private fun checkPermissionsAndOpenCamera() {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                openCamera()
+            }
+
+            else -> {
+                Log.d("PhotoEditFragment", "Requesting camera permission")
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private fun openCamera() {
+        imageUri = createImageUri()
+        takePictureLauncher.launch(imageUri)
+    }
+
+    private fun createImageUri(): Uri {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "new_image.jpg")
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        }
+        val imageUri = requireContext().contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues
+        )
+        return imageUri ?: throw IllegalStateException("Failed to create image URI")
+    }
+
+    private fun openGallery() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_READ_EXTERNAL_STORAGE
+            )
+        } else {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            resultLauncher.launch(intent)
+        }
+    }
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                data?.data?.let { uri ->
+                    imageUri = uri
+                    binding.ivProfile.setImageURI(uri)
+                }
+            }
+        }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_READ_EXTERNAL_STORAGE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openGallery()
+        }
+    }
+
+    private fun observeViewModel() {
+        // Aquí puedes observar cambios en el ViewModel si es necesario
+    }
+
+    private fun hideKeyboard() {
+        val imm =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
+    }
+
+    companion object {
+        private const val REQUEST_READ_EXTERNAL_STORAGE = 100
+    }
 }
--- app/src/main/java/com/saulhervas/easychat/ui/settings/ProfileSettingsFragment.kt
+++ app/src/main/java/com/saulhervas/easychat/ui/settings/ProfileSettingsFragment.kt
@@ -1,23 +1,17 @@
 package com.saulhervas.easychat.ui.settings
 
 import android.annotation.SuppressLint
-import android.content.ContentValues
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.saulhervas.easychat.R
 import com.saulhervas.easychat.databinding.FragmentProfileSettingsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,9 +19,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class ProfileSettingsFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileSettingsBinding
-    private lateinit var takePictureLauncher: ActivityResultLauncher<Uri>
-    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
-    private lateinit var imageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,84 +30,23 @@ class ProfileSettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProfileSettingsBinding.inflate(inflater, container, false)
-        Log.d("ProfileSettingsFragment", "View created")
-        setupCamera()
         setOnClickListener()
 
         setupUI(binding.root)
         return binding.root
     }
 
-    private fun setupCamera() {
-        takePictureLauncher =
-            registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
-                if (success) {
-                    Log.d("ProfileSettingsFragment", "Picture taken successfully")
-                    binding.ivProfile.setImageURI(imageUri)
-                } else {
-                    Log.d("ProfileSettingsFragment", "Failed to take picture")
-                    Toast.makeText(requireContext(), "Failed to take picture", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
-
-        requestPermissionLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-                if (isGranted) {
-                    Log.d("ProfileSettingsFragment", "Camera permission granted")
-                    openCamera()
-                } else {
-                    Log.d("ProfileSettingsFragment", "Camera permission denied")
-                    Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
-
     private fun setOnClickListener() {
         binding.tvEditPhoto.setOnClickListener {
-            Log.d("ProfileSettingsFragment", "tvEditPhoto clicked")
-            checkPermissionsAndOpenCamera()
+            findNavController().navigate(R.id.action_profileSettingsFragment_to_photoEditFragment)
         }
         binding.ivProfile.setOnClickListener {
             Log.d("ProfileSettingsFragment", "ivProfile clicked")
-            checkPermissionsAndOpenCamera()
+            findNavController().navigate(R.id.action_profileSettingsFragment_to_photoEditFragment)
         }
         binding.imBtnBack.setOnClickListener {
             findNavController().popBackStack()
         }
-    }
-
-    private fun checkPermissionsAndOpenCamera() {
-        when {
-            ContextCompat.checkSelfPermission(
-                requireContext(),
-                android.Manifest.permission.CAMERA
-            ) == android.content.pm.PackageManager.PERMISSION_GRANTED -> {
-                openCamera()
-            }
-
-            else -> {
-                Log.d("ProfileSettingsFragment", "Requesting camera permission")
-                requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
-            }
-        }
-    }
-
-    private fun openCamera() {
-        imageUri = createImageUri()
-        takePictureLauncher.launch(imageUri)
-    }
-
-    private fun createImageUri(): Uri {
-        val contentValues = ContentValues().apply {
-            put(MediaStore.Images.Media.DISPLAY_NAME, "new_image.jpg")
-            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
-        }
-        val imageUri = requireContext().contentResolver.insert(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            contentValues
-        )
-        return imageUri ?: throw IllegalStateException("No se pudo crear URI para la imagen")
     }
 
     private fun observeViewModel() {
